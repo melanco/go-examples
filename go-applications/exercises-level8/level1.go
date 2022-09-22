8387ef60aa05da27cd871787b7656d5e551bf911
omelancon <melanconolivier@gmail.com>
2022-09-21 21:46:32 -0400

    Quelques exercises

---
+++ go-applications/exercises-level8/level1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type user struct {
+	First string
+	Age   int
+}
+
+func main() {
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
+
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	//func Marshal(v any) ([]byte, error)
+
+	b, err := json.Marshal(users)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	os.Stdout.Write(b)
+
+}
