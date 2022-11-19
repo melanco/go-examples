f63db87919a0e5fb869a8d9299579c7afd47892e
omelancon <melanconolivier@gmail.com>
2022-11-19 14:59:00 -0500

    test

--- levelup-project/main.go
+++ levelup-project/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var version = "v0"
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website! Version %s", version)
+	})
+	fmt.Printf("App is starting, version: %s \n", version)
+	log.Fatal(http.ListenAndServe(":8500", nil))
 }
